fe56b9a6b6f564d5d947efc5104d2c3810a58ea6
EC2 Default User <ec2-user@ip-172-31-67-224.ec2.internal>
2018-12-11 10:26:00 +0000

    companyinfopage

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* global $ */
+
+$(function(){
+  $(document).ready(function(){
+    
+    $("#sp_navi_btn").click(function(){
+      
+      $(this).next().slideToggle();
+      
+    });
+    
+  });
+});
